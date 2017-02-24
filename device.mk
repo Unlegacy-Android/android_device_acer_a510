Unlegacy-Android/android_device_acer_a700

Unify rootdir filenames (2/2)

device.mk
@@ -1,12 +1,9 @@
 # USB config contains product-specific USB id
 PRODUCT_COPY_FILES += \
-    device/acer/a700/prebuilt/ramdisk/init.acer.usb.rc:root/init.acer.usb.rc
+    device/acer/a700/prebuilt/ramdisk/init.picasso.usb.rc:root/init.picasso.usb.rc
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240
-
-# So init files are copied properly in device_base.mk
-PRODUCT_BOOTLOADER := picasso_mf
 
 # Inherit t30-common
 $(call inherit-product, device/acer/t30-common/device_base.mk)

Unlegacy-Android/android_device_acer_a510

Change-Id: Idf88184b6a84fd91b2a8dee257b6525e068cef76

device.mk
@@ -3,13 +3,10 @@ PRODUCT_AAPT_PREF_CONFIG := xlarge mdpi
 
 # USB config contains product-specific USB id
 PRODUCT_COPY_FILES += \
-    device/acer/a510/prebuilt/ramdisk/init.acer.usb.rc:root/init.acer.usb.rc
+    device/acer/a510/prebuilt/ramdisk/init.picasso.usb.rc:root/init.picasso.usb.rc
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=160
-
-# So init files are copied properly in device_base.mk
-PRODUCT_BOOTLOADER := picasso_m
 
 PRODUCT_COPY_FILES += \
     device/acer/a510/prebuilt/ramdisk/init.picasso_m.rc:root/init.picasso_m.rc \