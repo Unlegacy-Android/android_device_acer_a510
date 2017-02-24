Unlegacy-Android/android_device_acer_a700

Set up for AOSP builds

AndroidProducts.mk
@@ -1,3 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_a700.mk \
-    $(LOCAL_DIR)/omni_a700.mk
+    $(LOCAL_DIR)/aosp_a700.mk

vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo omni_a700-userdebug
+add_lunch_combo aosp_a700-userdebug

Unlegacy-Android/android_device_acer_a510

Change-Id: Ic12ea97667da8024690633788be413a6a1649f75

AndroidProducts.mk
@@ -1,3 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_a510.mk \
-    $(LOCAL_DIR)/omni_a510.mk
+    $(LOCAL_DIR)/aosp_a510.mk

vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo omni_a510-userdebug
+add_lunch_combo aosp_a510-userdebug