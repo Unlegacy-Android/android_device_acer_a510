5afe2dd7943ed04ed703e3c4cc0ec333d94e5d24
elibl <elischa.blaess@gmail.com>
2017-02-24 21:35:24 +0100

    Revert "initial lvm"
    This reverts commit d5dc330e726bcc684aa9b03bcfe2be175713beff.
    
    Change-Id: I419463d10e0d5eb585163aef0bfbc8cd6c912888

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,3 @@
-# Inherit from t30-common
-include device/acer/t30-common/BoardConfigCommon.mk
-
 TARGET_BOOTLOADER_BOARD_NAME := picasso_m
 TARGET_OTA_ASSERT_DEVICE := picasso_m,a510_pa_cus
 
@@ -10,5 +7,5 @@ TARGET_KERNEL_CONFIG := picasso_m_defconfig
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/acer/a510/bluetooth
 
-# Recovery
-TARGET_RECOVERY_FSTAB := device/acer/a510/prebuilt/ramdisk/fstab.acer
+# Inherit from t30-common
+include device/acer/t30-common/BoardConfigCommon.mk
--- device.mk
+++ device.mk
@@ -8,18 +8,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=160
 
-PRODUCT_COPY_FILES += \
-    device/acer/a510/prebuilt/ramdisk/init.picasso_m.rc:root/init.picasso_m.rc \
-    device/acer/a510/prebuilt/ramdisk/init.recovery.picasso_m.rc:root/init.recovery.picasso_m.rc \
-    device/acer/a510/prebuilt/ramdisk/fstab.acer:root/fstab.acer \
-    device/acer/a510/prebuilt/ramdisk/fstab.acer:root/fstab.picasso_m \
-    device/acer/a510/prebuilt/ramdisk/twrp.fstab:recovery/root/etc/twrp.fstab
-
-# lvm files
-PRODUCT_COPY_FILES += \
-    device/acer/a510/prebuilt/ramdisk/lvm/etc/lvm.conf:root/lvm/etc/lvm.conf \
-    device/acer/a510/prebuilt/ramdisk/lvm/sbin/lvm:root/lvm/sbin/lvm \
-    device/acer/a510/prebuilt/ramdisk/lvm/sbin/setuplvmsystem.sh:recovery/root/lvm/sbin/setuplvmsystem.sh
-
 # Inherit t30-common
 $(call inherit-product, device/acer/t30-common/device_base.mk)
