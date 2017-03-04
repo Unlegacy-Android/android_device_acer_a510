baf75f78e2d266a6481abcecbeacb7be9081c36d
Ziyan <jaraidaniel@gmail.com>
2017-03-04 14:09:04 +0100

    Remove kernel config, add a510 to TARGET_OTA_ASSERT_DEVICE
    Change-Id: I73e7078b00f2d7169c3aea8a1fb7c7af0a632913

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,8 +1,5 @@
 TARGET_BOOTLOADER_BOARD_NAME := picasso_m
-TARGET_OTA_ASSERT_DEVICE := picasso_m,a510_pa_cus
-
-# Kernel defconfig
-TARGET_KERNEL_CONFIG := picasso_m_defconfig
+TARGET_OTA_ASSERT_DEVICE := a510,picasso_m,a510_pa_cus
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/acer/a510/bluetooth
